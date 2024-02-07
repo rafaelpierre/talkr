3f068435c15b03d9ff68f8522e93ae4ae9bd3dc3
Rafael <rafael.pierre@gmail.com>
2024-02-07 20:24:42 +0100

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use warp::Filter;
+use actix_files as fs;
+use actix_web::{web, App, HttpServer, Result};
 
-#[tokio::main]
-async fn main() {
-    // Serve static files from the `web` directory under the `/static` path
-    let static_files = warp::path("static").and(warp::fs::dir("web/"));
+async fn index() -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("web/index.html")?)
+}
 
-    // Serve `index.html` at the root path
-    let root = warp::path::end().and(warp::fs::file("web/index.html"));
-
-    // Combine the filters
-    let routes = root.or(static_files);
-
-    // Start the server
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/static", "web").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:3030")?
+    .run()
+    .await
 }
