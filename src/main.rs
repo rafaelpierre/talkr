f9e7b6ea37ea340ba8eed8de17a512459a08a788
Rafael <rafael.pierre@gmail.com>
2024-02-08 11:04:56 +0100

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_files as fs;
 use actix_web::{web, App, HttpServer, Result};
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+use std::env;
 
 async fn index() -> Result<fs::NamedFile> {
     Ok(fs::NamedFile::open("web/index.html")?)
@@ -7,6 +10,11 @@ async fn index() -> Result<fs::NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    env::set_var("RUST_LOG", "INFO");
+    pretty_env_logger::init();
+    info!("Starting talkr");
+
     HttpServer::new(|| {
         App::new()
             .service(fs::Files::new("/static", "web").show_files_listing())
